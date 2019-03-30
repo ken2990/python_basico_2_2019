5b03b78ff7ecca99cb7326c17dcbd46b600ddd4a
ken2990 <49084357+ken2990@users.noreply.github.com>
2019-03-30 12:16:35 -0600

    Add files via upload
    Practicas semana 1

---
+++ Ken2.py
@@ -0,0 +1,20 @@
+# comentario de practica para 2 lineas
+
+'''
+comentario en multiples lineas
+'''
+
+print('hola a todos!')
+print('buenos dias')
+
+#varias lineas un solo print solo con coma.error
+
+print('mi primera linea', 'mi segunda linea')
+
+#varias linea un solo print con \n
+
+print('primera linea\nmi segunda linea')
+
+
+print('hola de nuevo')
+
---
+++ Ken3.py
@@ -0,0 +1,19 @@
+# para revisar los tipos de datos
+
+#numero entero
+#definir numero entero
+
+123456789
+
+mi_variable = 123456789
+
+print('el valor de la variable llamada "mi_variable es"', mi_variable)
+
+#caso1
+print('el valor de la variable llamada "mi_variable es"', mi_variable)
+
+#caso2
+print("el valor de la variable llamada 'mi_variable es'", mi_variable)
+
+#caso 3
+print("el valor de la variable llamada \"mi_variable es\"", mi_variable)
